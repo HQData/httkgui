e9e503a6ebe54a87cccd847fa0c81120e0fd2c47
Witold Wiecek <wwiecek@la-ser.com>
2017-10-30 14:20:24 +0000

    C code for 1 compound cleaned up (prep for mixtures)

--- src/vLiverPBPK_3cl.c
+++ src/vLiverPBPK_3cl.c
@@ -1,9 +1,11 @@
 /* 
- This is modification of vLiverPBPK .c program with addition of new metabolism parameters
+ This is modification of vLiverPBPK .c program with addition of some parameters
  
- New parameters are compartment-specific CL parameter, Vmax, km.
- * If Vmax & km are available (by passing km != 0) we use them for calculation
- * Otherwise CL parameter used
+ New parameters are compartment-specific CL parameters or Vmax, km.
+ If CL is to be used, km should be supplied as 0. If km != 0 we automatically use Vmax and km.
+ 
+ Code has been cleaned up and reorganised.
+ C1_ prefix is added for convenience to expand the script to mixtures (see separate script).
  
  Author of the modification: WW / LASER
 */
@@ -11,81 +13,79 @@
 #include <R.h>
 
 /* Model variables: States */
-#define ID_Agutlumen 0x0000
-#define ID_Agut 0x0001
-#define ID_Aliver 0x0002
-#define ID_Aven 0x0003
-#define ID_Alung 0x0004
-#define ID_Aart 0x0005
-#define ID_Arest 0x0006
-#define ID_Akidney 0x0007
-#define ID_Atubules 0x0008
-#define ID_Ametabolized 0x0009
-#define ID_AUC 0x000a
+#define C1_ID_Agutlumen    0x0000
+#define C1_ID_Agut         0x0001
+#define C1_ID_Aliver       0x0002
+#define C1_ID_Aven         0x0003
+#define C1_ID_Alung        0x0004
+#define C1_ID_Aart         0x0005
+#define C1_ID_Arest        0x0006
+#define C1_ID_Akidney      0x0007
+#define C1_ID_Atubules     0x0008
+#define C1_ID_Ametabolized 0x0009
+#define C1_ID_AUC          0x000a
 
 /* Model variables: Outputs */
-#define ID_Cgut 0x0000
-#define ID_Cliver 0x0001
-#define ID_Cven 0x0002
-#define ID_Clung 0x0003
-#define ID_Cart 0x0004
-#define ID_Crest 0x0005
-#define ID_Ckidney 0x0006
-#define ID_Cserum 0x0007
-#define ID_Aserum 0x0008
+#define C1_ID_Cgut         0x0000
+#define C1_ID_Cliver       0x0001
+#define C1_ID_Cven         0x0002
+#define C1_ID_Clung        0x0003
+#define C1_ID_Cart         0x0004
+#define C1_ID_Crest        0x0005
+#define C1_ID_Ckidney      0x0006
+#define C1_ID_Cserum       0x0007
+#define C1_ID_Aserum       0x0008
 
-/* Parameters  */
+
 static double parms[45];
 
-#define BW parms[0]
-#define CLmetabolismc parms[1]
-#define hematocrit parms[2]
-#define kgutabs parms[3]
-#define Kkidney2plasma parms[4]
-#define Kliver2plasma parms[5]
-#define Krest2plasma parms[6]
-#define Kgut2plasma parms[7]
-#define Klung2plasma parms[8]
-#define Qcardiacc parms[9]
-#define Qgfrc parms[10]
-#define Qgutf parms[11]
-#define Qkidneyf parms[12]
-#define Qliverf parms[13]
-#define Vartc parms[14]
-#define Vgutc parms[15]
-#define Vkidneyc parms[16]
-#define Vliverc parms[17]
-#define Vlungc parms[18]
-#define Vrestc parms[19]
-#define Vvenc parms[20]
-#define Fraction_unbound_plasma parms[21]
-#define Ratioblood2plasma parms[22]
-#define CLmetabolism parms[23]
-#define Qcardiac parms[24]
-#define Qgfr parms[25]
-#define Qgut parms[26]
-#define Qkidney parms[27]
-#define Qliver parms[28]
-#define Qrest parms[29]
-#define Vart parms[30]
-#define Vgut parms[31]
-#define Vkidney parms[32]
-#define Vliver parms[33]
-#define Vlung parms[34]
-#define Vrest parms[35]
-#define Vven parms[36]
+/* Parameters */
+#define C1_BW parms[0]
+#define C1_CLmetabolismc parms[1]
+#define C1_hematocrit parms[2]
+#define C1_kgutabs parms[3]
+#define C1_Kkidney2plasma parms[4]
+#define C1_Kliver2plasma parms[5]
+#define C1_Krest2plasma parms[6]
+#define C1_Kgut2plasma parms[7]
+#define C1_Klung2plasma parms[8]
+#define C1_Qcardiacc parms[9]
+#define C1_Qgfrc parms[10]
+#define C1_Qgutf parms[11]
+#define C1_Qkidneyf parms[12]
+#define C1_Qliverf parms[13]
+#define C1_Vartc parms[14]
+#define C1_Vgutc parms[15]
+#define C1_Vkidneyc parms[16]
+#define C1_Vliverc parms[17]
+#define C1_Vlungc parms[18]
+#define C1_Vrestc parms[19]
+#define C1_Vvenc parms[20]
+#define C1_Fraction_unbound_plasma parms[21]
+#define C1_Ratioblood2plasma parms[22]
+#define C1_CLmetabolism parms[23]
+#define C1_Qcardiac parms[24]
+#define C1_Qgfr parms[25]
+#define C1_Qgut parms[26]
+#define C1_Qkidney parms[27]
+#define C1_Qliver parms[28]
+#define C1_Qrest parms[29]
+#define C1_Vart parms[30]
+#define C1_Vgut parms[31]
+#define C1_Vkidney parms[32]
+#define C1_Vliver parms[33]
+#define C1_Vlung parms[34]
+#define C1_Vrest parms[35]
+#define C1_Vven parms[36]
+#define C1_Vmax_liver parms[37]
+#define C1_Km_liver parms[38]
+#define C1_Vmax_gut parms[39]
+#define C1_Km_gut parms[40]
+#define C1_Vmax_kidney parms[41]
+#define C1_Km_kidney parms[42]
+#define C1_CLmetabolism_gut parms[43]
+#define C1_CLmetabolism_kidney parms[44]
 
-//new additions (LASER):
-#define Vmax_liver parms[37]
-#define Km_liver parms[38]
-#define Vmax_gut parms[39]
-#define Km_gut parms[40]
-#define Vmax_kidney parms[41]
-#define Km_kidney parms[42]
-#define CLmetabolism_gut parms[43]
-#define CLmetabolism_kidney parms[44]
-
-double lastterm; //for calculating this 'last term to subtract' 
 
 /*----- Initializers */ 
 void initmod_3cl (void (* odeparms)(int *, double *))
@@ -98,85 +98,69 @@ void initmod_3cl (void (* odeparms)(int *, double *))
 
 void derivs_3cl (int *neq, double *pdTime, double *y, double *ydot, double *yout, int *ip)
 {
-  yout[ID_Cgut] = y[ID_Agut] / Vgut ;
-
-  yout[ID_Cliver] = y[ID_Aliver] / Vliver ;
-
-  yout[ID_Cven] = y[ID_Aven] / Vven ;
-
-  yout[ID_Clung] = y[ID_Alung] / Vlung ;
-
-  yout[ID_Cart] = y[ID_Aart] / Vart ;
-
-  yout[ID_Crest] = y[ID_Arest] / Vrest ;
-
-  yout[ID_Ckidney] = y[ID_Akidney] / Vkidney ;
-
-  yout[ID_Cserum] = y[ID_Aven] / Vven / Ratioblood2plasma ; 
+  double ametabolised_liver; double ametabolised_kidney; double ametabolised_gut;
+  
+  //concentrations (amount/volume)
+  yout[C1_ID_Cgut] = y[C1_ID_Agut] / C1_Vgut ;
+  yout[C1_ID_Cliver] = y[C1_ID_Aliver] / C1_Vliver ;
+  yout[C1_ID_Cven] = y[C1_ID_Aven] / C1_Vven ;
+  yout[C1_ID_Clung] = y[C1_ID_Alung] / C1_Vlung ;
+  yout[C1_ID_Cart] = y[C1_ID_Aart] / C1_Vart ;
+  yout[C1_ID_Crest] = y[C1_ID_Arest] / C1_Vrest ;
+  yout[C1_ID_Ckidney] = y[C1_ID_Akidney] / C1_Vkidney ;
+  yout[C1_ID_Cserum] = y[C1_ID_Aven] / C1_Vven / C1_Ratioblood2plasma ; 
   //(ratioblood2serum the same as for plasma)
-
-  yout[ID_Aserum] = y[ID_Aven] / Ratioblood2plasma * ( 1 - hematocrit ) ;
-
-  ydot[ID_Agutlumen] = - kgutabs * y[ID_Agutlumen] ;
-
-  /* Agut flow: modified by subtracting term (lastterm) proportional to gut-specific metabolism */
-  if(Km_gut==0) {
-      lastterm = CLmetabolism_gut * y[ID_Agut] / Vgut / Kgut2plasma ;
-  } else {
-      lastterm = (Vmax_gut * y[ID_Agut] / Vgut / Kgut2plasma) / (Km_gut + y[ID_Agut] / Vgut / Kgut2plasma);
-  }
-  ydot[ID_Agut] = kgutabs * y[ID_Agutlumen] + Qgut * ( y[ID_Aart] / Vart - y[ID_Agut] / Vgut * Ratioblood2plasma / Kgut2plasma / Fraction_unbound_plasma ) - lastterm;
-  /* End Agut flow. */
+  yout[C1_ID_Aserum] = y[C1_ID_Aven] / C1_Ratioblood2plasma * ( 1 - C1_hematocrit ) ;
+  ydot[C1_ID_Agutlumen] = - C1_kgutabs * y[C1_ID_Agutlumen] ;
+  
+  //start flows with calculation of amount metabolised in each of 3 organs: liver, kidney and gut
+  //it will depend on whether Km is provided (as != 0)
+  
+  if(C1_Km_liver != 0) 
+    ametabolised_liver  = (C1_Vmax_liver * y[C1_ID_Aliver] / C1_Vliver / C1_Kliver2plasma) / 
+      (C1_Km_liver + y[C1_ID_Aliver] / C1_Vliver / C1_Kliver2plasma);
+  else 
+    ametabolised_liver  = (C1_CLmetabolism        * y[C1_ID_Aliver] / C1_Vliver / C1_Kliver2plasma);
+  
+  if(C1_Km_kidney!=0)
+    ametabolised_kidney = (C1_Vmax_kidney * y[C1_ID_Akidney] / C1_Vkidney / C1_Kkidney2plasma) / 
+      (C1_Km_kidney + y[C1_ID_Akidney] / C1_Vkidney / C1_Kkidney2plasma);
+  else
+    ametabolised_kidney = (C1_CLmetabolism_kidney * y[C1_ID_Akidney] / C1_Vkidney / C1_Kkidney2plasma);
+  
+  if(C1_Km_gut != 0)
+    ametabolised_gut = (C1_Vmax_gut * y[C1_ID_Agut] / C1_Vgut / C1_Kgut2plasma) / 
+      (C1_Km_gut + y[C1_ID_Agut] / C1_Vgut / C1_Kgut2plasma);
+  else
+    ametabolised_gut    = (C1_CLmetabolism_gut    * y[C1_ID_Agut] / C1_Vgut / C1_Kgut2plasma);
+  
+  /* Agut flow: modified by subtracting term gut-specific metabolism */
+  ydot[C1_ID_Agut] = C1_kgutabs * y[C1_ID_Agutlumen] + C1_Qgut * ( y[C1_ID_Aart] / C1_Vart - y[C1_ID_Agut] / C1_Vgut * C1_Ratioblood2plasma / C1_Kgut2plasma / C1_Fraction_unbound_plasma );
+  ydot[C1_ID_Agut] -= ametabolised_gut;
+  
+  /* Aliver flow: modified by subtracting liver-specific metabolism */
+  ydot[C1_ID_Aliver] = C1_Qliver * y[C1_ID_Aart] / C1_Vart + C1_Qgut * y[C1_ID_Agut] / C1_Vgut * C1_Ratioblood2plasma / C1_Kgut2plasma / C1_Fraction_unbound_plasma - 
+  ( C1_Qliver + C1_Qgut ) * y[C1_ID_Aliver] / C1_Vliver / C1_Kliver2plasma / C1_Fraction_unbound_plasma * C1_Ratioblood2plasma;
+  ydot[C1_ID_Aliver] -= ametabolised_liver;
   
   
-  /* Aliver flow: modified by subtracting term (lastterm) proportional to liver-specific metabolism */
-  if(Km_liver==0) {
-      lastterm = CLmetabolism * y[ID_Aliver] / Vliver / Kliver2plasma ;
-  } else {
-      lastterm = (Vmax_liver * y[ID_Aliver] / Vliver / Kliver2plasma) / (Km_liver + y[ID_Aliver] / Vliver / Kliver2plasma);
-  }
-  ydot[ID_Aliver] = Qliver * y[ID_Aart] / Vart + Qgut * y[ID_Agut] / Vgut * Ratioblood2plasma / Kgut2plasma / Fraction_unbound_plasma - 
-    ( Qliver + Qgut ) * y[ID_Aliver] / Vliver / Kliver2plasma / Fraction_unbound_plasma * Ratioblood2plasma - lastterm;
-  /* End Aliver flow. */
+  ydot[C1_ID_Aven]  = ( ( C1_Qliver + C1_Qgut ) * y[C1_ID_Aliver] / C1_Vliver / C1_Kliver2plasma + C1_Qkidney * y[C1_ID_Akidney] / C1_Vkidney / C1_Kkidney2plasma + 
+    C1_Qrest * y[C1_ID_Arest] / C1_Vrest / C1_Krest2plasma ) * C1_Ratioblood2plasma / C1_Fraction_unbound_plasma - C1_Qcardiac * y[C1_ID_Aven] / C1_Vven ;
+  ydot[C1_ID_Alung] = C1_Qcardiac * ( y[C1_ID_Aven]  / C1_Vven - y[C1_ID_Alung] / C1_Vlung * C1_Ratioblood2plasma / C1_Klung2plasma / C1_Fraction_unbound_plasma ) ;
+  ydot[C1_ID_Aart]  = C1_Qcardiac * ( y[C1_ID_Alung] / C1_Vlung * C1_Ratioblood2plasma / C1_Klung2plasma / C1_Fraction_unbound_plasma - y[C1_ID_Aart] / C1_Vart ) ;
+  ydot[C1_ID_Arest] = C1_Qrest    * ( y[C1_ID_Aart]  / C1_Vart - y[C1_ID_Arest] / C1_Vrest * C1_Ratioblood2plasma / C1_Krest2plasma / C1_Fraction_unbound_plasma ) ;
   
-  ydot[ID_Aven] = ( ( Qliver + Qgut ) * y[ID_Aliver] / Vliver / Kliver2plasma + Qkidney * y[ID_Akidney] / Vkidney / Kkidney2plasma + 
-  Qrest * y[ID_Arest] / Vrest / Krest2plasma ) * Ratioblood2plasma / Fraction_unbound_plasma - Qcardiac * y[ID_Aven] / Vven ;
-
-  ydot[ID_Alung] = Qcardiac * ( y[ID_Aven] / Vven - y[ID_Alung] / Vlung * Ratioblood2plasma / Klung2plasma / Fraction_unbound_plasma ) ;
-
-  ydot[ID_Aart] = Qcardiac * ( y[ID_Alung] / Vlung * Ratioblood2plasma / Klung2plasma / Fraction_unbound_plasma - y[ID_Aart] / Vart ) ;
-
-  ydot[ID_Arest] = Qrest * ( y[ID_Aart] / Vart - y[ID_Arest] / Vrest * Ratioblood2plasma / Krest2plasma / Fraction_unbound_plasma ) ;
-
-  /* Akidney flow: modified by subtracting term (lastterm) proportional to liver-specific metabolism */
-  if(Km_kidney==0) {
-    lastterm = - CLmetabolism_kidney * y[ID_Akidney] / Vkidney / Kkidney2plasma;
-    ydot[ID_Akidney] = Qkidney * y[ID_Aart] / Vart - Qkidney * y[ID_Akidney] / Vkidney / Kkidney2plasma * Ratioblood2plasma / Fraction_unbound_plasma - 
-      Qgfr * y[ID_Akidney] / Vkidney / Kkidney2plasma - lastterm;
-  } else {
-    //here not just subtracting an extra term but instead of 'Qgfr term' in the above (and sans extra subtraction):
-    ydot[ID_Akidney] = Qkidney * y[ID_Aart] / Vart - Qkidney * y[ID_Akidney] / Vkidney / Kkidney2plasma * Ratioblood2plasma / Fraction_unbound_plasma - 
-      ((Vmax_kidney * y[ID_Akidney])/(Km_kidney + y[ID_Akidney]));
-  }
+  /* Akidney flow: modified by subtracting Qgfr and liver-specific metabolism */
+  ydot[C1_ID_Akidney]  = C1_Qkidney * y[C1_ID_Aart] / C1_Vart - C1_Qkidney * y[C1_ID_Akidney] / C1_Vkidney / C1_Kkidney2plasma * C1_Ratioblood2plasma / C1_Fraction_unbound_plasma;
+  ydot[C1_ID_Akidney] -= C1_Qgfr * y[C1_ID_Akidney] / C1_Vkidney / C1_Kkidney2plasma; //see Atubules
+  ydot[C1_ID_Akidney] -= ametabolised_kidney;
   /* End Akidney flow. */
   
-
-  ydot[ID_Atubules] = Qgfr * y[ID_Akidney] / Vkidney / Kkidney2plasma ;
+  ydot[C1_ID_Atubules] = C1_Qgfr * y[C1_ID_Akidney] / C1_Vkidney / C1_Kkidney2plasma ;
   
-  /* Modification: CL partitioned into three terms */
-  if(Km_kidney!=0 && Km_gut != 0 && Km_liver != 0) {
-    ydot[ID_Ametabolized] = 
-      (Vmax_liver * y[ID_Aliver] / (Km_liver + y[ID_Aliver])) + 
-      (Vmax_kidney * y[ID_Akidney] / (Km_kidney + y[ID_Akidney])) + 
-      (Vmax_gut * y[ID_Agut] / (Km_gut + y[ID_Agut])); 
-      
-  } else {
-    ydot[ID_Ametabolized] = 
-      (CLmetabolism * y[ID_Aliver] / Vliver / Kliver2plasma) + 
-      (CLmetabolism_kidney * y[ID_Akidney] / Vkidney / Kkidney2plasma) + 
-      (CLmetabolism_gut * y[ID_Agut] / Vgut / Kgut2plasma);
-  }
-  ydot[ID_AUC] = y[ID_Aven] / Vven / Ratioblood2plasma ; 
-  //(not very helpful here or used to calculate later?)
-
+  /* Modification: contribution to amount metabolised partitioned into three organs */
+  ydot[C1_ID_Ametabolized] = ametabolised_liver + ametabolised_gut + ametabolised_kidney;
+  
+  ydot[C1_ID_AUC] = y[C1_ID_Aven] / C1_Vven / C1_Ratioblood2plasma ; 
+  
 } /* derivs */
